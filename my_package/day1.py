23de5b60e2e9e3f9b74dbcb6d93047fa9c35251f
Vicky Banks <victoria.banks052@gmail.com>
2021-12-03 18:31:19 +0000

    example from Kate

---
+++ my_package/day1.py
@@ -0,0 +1,9 @@
+def my_function():
+    print('I am inside this function')
+
+
+if __name__ == '__main__':
+    print('I am in the main part of the file')
+    print('I am below the function but outside main')
+    ##any code in here won't be run if this file is imported
+
---
+++ my_package/run.py
@@ -0,0 +1,7 @@
+from my_package.day1 import my_function
+
+print(my_function())
+fruits = ["apple", "banana"]
+my_dictionary = {"fruit": "apple"}
+
+
