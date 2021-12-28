0e725fe921c1051a6563f86d969353d5b2ae9114
Vicky Banks <victoria.banks052@gmail.com>
2021-12-28 10:13:52 +0000

    Altered code to account for change of fuel rule in part 2.

--- d7_hungry_whale.R
+++ d7_hungry_whale.R
@@ -4,6 +4,8 @@ library(readr)
 
 ## Task - the crabs must move to align with one position moved using one unit of fuel. 
 ## You need to find the position that uses the least fuel for all crabs to get there
+## Task 1 - one move = one until of fuel
+## Task 2 - each move uses one more unitl of fuel i.e first move = 1, second move = 2, so to move 2 positions uses 1+2=3 units
 
 crabs<- data.frame(position = c(16,1,2,0,4,2,7,1,2,14))
 crabs<- read_csv("d7_hungry_whale.csv")
@@ -23,7 +25,9 @@ for(align_position in min_pos:max_pos) {
   fuel_total <- 0
   ## find the fuel for each crab to get there
   for (crab in 1:nrow(crabs)) {
-    fuel_used <- abs(crabs$position[crab] - align_position)
+    
+    moves<-abs(crabs$position[crab] - align_position)
+    fuel_used <- 0.5*(moves*moves + moves)
     fuel_total <- fuel_total + fuel_used
   }
   ##put in df
